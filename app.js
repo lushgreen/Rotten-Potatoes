32d9a2a9e6eda909716d1ea1fbb9c1336130b091
lushgreen <xscapesoft@yahoo.co.uk>
2025-05-19 21:47:11 +0100

    Users can see all reviews

--- app.js
+++ app.js
@@ -12,20 +12,27 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-*/
 
 app.get('/', (req, res) => {
   res.render('home', { msg: 'Handlebars are Cool!' });
 })
+*/
 
 // OUR MOCK ARRAY OF PROJECTS
 let reviews = [
   { title: "Great Review", movieTitle: "Batman II" },
-  { title: "Awesome Movie", movieTitle: "Titanic" }
+  { title: "Awesome Movie", movieTitle: "Titanic" },
+  { title: "My Movie Review", movieTitle: "Transformers: Megatron" }
 ]
 
 // INDEX
+/*
 app.get('/reviews', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
+})
+*/
+
+app.get('/', (req, res) => {
   res.render('reviews-index', { reviews: reviews });
 })
 
