cad6d0397b1b9c138a8ecf5b93127b9ba56a0405
lushgreen <xscapesoft@yahoo.co.uk>
2025-05-19 20:15:39 +0100

    init

--- app.js
+++ app.js
@@ -18,6 +18,17 @@ app.get('/', (req, res) => {
   res.render('home', { msg: 'Handlebars are Cool!' });
 })
 
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review", movieTitle: "Batman II" },
+  { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
+// INDEX
+app.get('/reviews', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
+})
+
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
